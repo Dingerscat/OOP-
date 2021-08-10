89b795f50ea5ca5c5fb942970d05a0b9e24125d6
Dingerscat <66262663+Dingerscat@users.noreply.github.com>
2021-08-10 22:41:05 +0800

    Create GlobalFile.h

---
+++ GlobalFile.h
@@ -0,0 +1,10 @@
+#pragma once
+
+//图书信息文件
+constexpr auto BOOK_FILE = "Book.txt";
+
+//借书人文件
+constexpr auto BORROWER_FILE = "Borrower.txt";
+
+//借阅关系文件
+constexpr auto RELATION_FILE = "Relation.txt";
